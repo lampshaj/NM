8d9ed924579b96c2ca9b2be3513fa77701a6964c
unknown <lampshaj@gmail.com>
2018-12-24 11:00:57 -0600

    test revision

--- src/sort.test.js
+++ src/sort.test.js
@@ -1,21 +1,20 @@
 import * as s from './helpers';
 
 test('test sortItems function', () => {
-  let c1 = {
+  const c1 = {
     name: "Aaron",
     speed: "50"
   }
-  let c2 = {
+  const c2 = {
     name: "Jerry",
     speed: "1000"
   }
-  let c3 = {
+  const c3 = {
     name: "Tom",
     speed: "100"
   }
-  let items = [c3, c2, c1];
-  let sorted = ["Aaron", "Jerry", "Tom"];
-  let returnList = [];
-  returnList = s.sortItems(items);
-  expect(returnList[0]).toEqual(sorted[0]);
+  const items = [c3, c2, c1];
+  const sorted = ["Aaron", "Jerry", "Tom"];
+  const result = s.sortItems(items);
+  expect(result).toEqual(sorted);
 });
